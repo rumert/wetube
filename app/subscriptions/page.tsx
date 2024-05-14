b07b50961caea195d0fcb7d8e0f8b488059cb2f9
rumert <154933062+rumert@users.noreply.github.com>
2024-05-14 14:49:40 +0300

    added page

---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,12 @@
+import SubscriptionAvatars from '@/components/SubscriptionAvatars'
+import VideoListing from '@/components/VideoListing'
+import React from 'react'
+
+export default function subscriptions() {
+  return (
+    <div>
+        <SubscriptionAvatars />
+        <VideoListing />
+    </div>
+  )
+}
---
+++ components/SubscriptionAvatars.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function SubscriptionAvatars() {
+    return(
+    <div className="avatar-group -space-x-6 rtl:space-x-reverse py-8 ml-24 px-12">
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar placeholder">
+        <div className="w-12 bg-neutral text-neutral-content">
+          <span>+24</span>
+        </div>
+      </div>
+    </div>
+    )
+}
